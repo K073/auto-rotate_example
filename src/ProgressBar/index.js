84a86d370a455761bd0f023933c9215ca80e53e3
k073 <razzztaedd@gmail.com>
2019-07-24 06:24:09 +0600

    first commit

---
+++ src/ProgressBar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const ProgressBar = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+ProgressBar.propTypes = {
+
+};
+
+export default ProgressBar;
